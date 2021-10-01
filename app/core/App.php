50de1daef03b7b7a1d0291ec7cfb837b8b23e3a1
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-10-01 15:21:42 +0700

    Mempelajari tentang routing dalam mvc dan juga access dari folder php (menyelesaikan video 3 routing)

--- app/core/App.php
+++ app/core/App.php
@@ -2,10 +2,22 @@
 
 // Aria Rupawansyah - 193040140
 
-class App{
-	
+class App {
+
     public function __construct()
     {
-        echo 'OK!';
+        $url = $this->parseURL();
+        var_dump($url);
     }
+
+    public function parseURL()
+    {
+        if( isset($_GET['url']) ){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+    
 } 
