79d5c0c67486673514b95c6eb321911e4808371b
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-10-01 15:24:51 +0700

    Mempelajari tentang controller, method, params dalam php mvc (menyelesaikan video 4 controller)

---
+++ app/controllers/About.php
@@ -0,0 +1,17 @@
+<?php
+
+// Aria Rupawansyah - 193040140
+
+class About{
+
+    public function index($nama = 'Aria', $pekerjaan = 'Mahasiswa')
+    {
+        echo "Halo, nama saya $nama, Saya adalah seorang $pekerjaan";
+    }
+
+    public function page()
+    {
+        echo 'About/page';
+    }
+
+}
---
+++ app/controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+// Aria Rupawansyah - 193040140
+
+class Home{
+
+    public function index()
+    {
+        echo 'home/index';
+    }
+
+}
